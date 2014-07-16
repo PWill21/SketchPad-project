9d3c7d4b667b062ee8441464d3987a3c1b593691
Patrick J. Will <patrickwill2121@gmail.com>
2014-07-16 18:30:30 -0400

    added createGrid() function

---
+++ sketch.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	// when document loads, call createGrid() function that takes a default parameter of size 16x16
+	createGrid(16);
+});
+
+function createGrid(size) {
+
+}
