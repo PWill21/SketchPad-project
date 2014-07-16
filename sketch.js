6d0620a285d260dd8739116318571fd825da67c3
Patrick J. Will <patrickwill2121@gmail.com>
2014-07-16 18:43:33 -0400

    changed createGrid() to take two parameters, width and height of grid

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 	// when document loads, call createGrid() function that takes a default parameter of size 16x16
-	createGrid(16);
+	createGrid(16, 16);
 });
 
-function createGrid(size) {
-
+function createGrid(gridWidth, gridHeight) {
+	// create divs; total number of divs is gridWidth * gridHeight
+	for(i = 1; i <= (gridWidth * gridHeight); i++) {
+		
+	}
 }
