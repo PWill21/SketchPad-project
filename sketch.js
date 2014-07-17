ca352a9a4bb3b98ec12209e5d476a87019149560
Patrick J. Will <patrickwill2121@gmail.com>
2014-07-17 02:13:15 -0400

    added changeColor() function

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 	// when document loads, call createGrid() function that takes a default parameter of size 16x16
 	createGrid(16, 16);
+	changeColor();
 });
 
+/************************************************************************************************
+											Functions
+************************************************************************************************/
 function createGrid(gridWidth, gridHeight) {
 	// create divs; total number of divs is gridWidth * gridHeight
 	for(var i = 1; i <= (gridWidth * gridHeight); i++) {
@@ -10,3 +14,10 @@ function createGrid(gridWidth, gridHeight) {
 		$("#container").append("<div class='box'></div>");
 	}
 }
+function changeColor() {
+	$(".box").mouseover(function() {
+		$(this).css("background-color", "#999")
+	});
+}
+
+
