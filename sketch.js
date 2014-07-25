7ef8230e024c384a6400d68aba139cc77e2add66
Patrick J. Will <patrickwill2121@gmail.com>
2014-07-25 01:40:48 -0400

    added removeGrid() function

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	// when document loads, call createGrid() function that takes a default parameter of size 16x16
 	createGrid(16, 16);
 	changeColor();
-	clearGrid();
+	removeGrid();
 });
 
 /************************************************************************************************
@@ -21,12 +21,10 @@ function changeColor() {
 	});
 }
 
-function clearGrid() {
-	$("#clear_button").click(function() {
-		newGridWidth = prompt("What size for new grid?"); // asks for width of new grid. will also be height
-		$(".box").css("background-color", "#FFF"); // clears grid of all color, simulates reset
-		var newCSSWidth = Math.floor(850/newGridWidth); // determines new width size for .box <div>s
-		$(".box").css("width", "newCSSWidth");
-		createGrid(newGridWidth, newGridWidth);
+function removeGrid() {
+	$("#clear_button").click(function () {
+		$("#container").remove();
 	});
+	createGrid(16,16);
 }
+
