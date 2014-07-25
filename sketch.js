cac92be87f994c4e2c7b4920f1e40bfcb89ecc9f
Patrick J. Will <patrickwill2121@gmail.com>
2014-07-25 02:18:04 -0400

    modified makeNewGrid() so that each new .box created is a perfect square

--- sketch.js
+++ sketch.js
@@ -30,11 +30,15 @@ function removeGrid() {
 
 function makeNewGrid() {
  	$("#make_new").click(function() {
-
  		var newSize = prompt("What is the new size?");
  		for(var i =1; i <= (newSize * newSize); i++) {
  			$("#container").append("<div class='box'></div>");
  		}
+ 		var newBoxWidth = Math.floor((850/newSize) - 2); /*calculates the size of each box based on width of <body> (850px). -2 accounts for size of border around each .box (1px). */
+ 		var newBoxHeight = newBoxWidth;
+ 		$(".box").css("width", newBoxWidth);
+ 		$(".box").css("height", newBoxHeight); /* assures that each new .box is in fact a perfect square */
+ 		changeColor();
  	});
 }
 
